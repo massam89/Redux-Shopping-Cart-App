582c6c358272e5577569fbfbf4b26acdcfaa08e3
Masoud <mas.sam1368@gmail.com>
2022-07-24 14:32:26 +0430

    first

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { cartAction } from "../store/cart-slice";
 
 import "./Product.css";
 const Product = ({ name, id, imgURL, price }) => {
+
+  const dispatch = useDispatch()
+  const addToCard = () => {
+    dispatch(cartAction.addToCart({
+      id,
+      name,
+      price
+    }))
+  }
   return (
     <div className="card">
       <img src={imgURL} alt={name} />
       <h2>{name}</h2>
       <p>$ {price}</p>
-      <button>Add to cart</button>
+      <button onClick={addToCard}>Add to cart</button>
     </div>
   );
 };
